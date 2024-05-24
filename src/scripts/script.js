7d53b3e1b4964119169ce21af3aedebaea41ae7a
João Emanuel <124364697+manukie@users.noreply.github.com>
2024-05-23 21:08:04 -0300

    feat: site totalmente feito e completo

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,38 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModal() {
+    const button_1 = document.querySelector("#showMessage");
+    const button_2 = document.querySelector("#openMessage");
+
+    const modalContainer = document.querySelector("#modalController");
+  
+    button_1.addEventListener("click", () => {
+      modalContainer.showModal();
+
+      closeModal();
+    });
+
+    button_2.addEventListener("click", () => {
+        modalContainer.showModal();
+
+        closeModal();
+      });
+  }
+
+  function closeModal() {
+    const button_1 = document.querySelector("#closeModal");
+    const button_2 = document.querySelector("#endModal");
+    const modalContainer = document.querySelector("#modalController");
+  
+    button_1.addEventListener("click", () => {
+      modalContainer.close();
+    });
+
+    button_2.addEventListener("click", () => {
+        modalContainer.close();
+      });
+  }
+  
+  
+  handleModal();
+
+
+  
